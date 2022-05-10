4df66dc5900839e1b0ebab348849dd0717f37af6
Ebo0 <enkhmanlai.enkhbold@tekschool.us>
2022-05-09 21:38:12 -0400

    Another class added

---
+++ src/AnotherGithubClass.java
@@ -0,0 +1,12 @@
+
+public class AnotherGithubClass {
+	public static void main(String[] args) {
+		String myGoal = "becoming a SDET";
+		int [] myArray = {1,3,4,5};
+		
+		
+		for(int i: myArray) {
+			System.out.println(myGoal + " " +i);
+		}
+	}
+}
